c0fba72312074d25b5fdc03d95384c63a5f67bcf
Daniel <danielzxcheng@gmail.com>
2025-07-16 16:51:56 -0400

    Reworked bullet logic to use PhysicsComponent

--- scripts/projectiles/bullet.gd
+++ scripts/projectiles/bullet.gd
@@ -1,21 +1,21 @@
-extends Node2D
+extends RigidBody2D
 
 
 @onready var hitbox_component: HitboxComponent = $HitboxComponent
+@onready var physics_component: PhysicsComponent = $PhysicsComponent
 
 var damage: float
 var knockback_force: float
 var movement_speed: float
 var max_range: float
 var travelled_distance: float = 0
-	
-	
-func _physics_process(delta: float) -> void:
-	
-	var direction : Vector2 = Vector2.RIGHT.rotated(rotation)
-	
-	position += direction * movement_speed * delta
-	travelled_distance += movement_speed * delta
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	var direction: Vector2 = Vector2.RIGHT.rotated(rotation).normalized()
+	physics_component.set_base_velocity(direction * movement_speed)
+	travelled_distance += movement_speed * state.step
+	state.linear_velocity = physics_component.velocity
 	
 	if travelled_distance > max_range:
 		queue_free()
